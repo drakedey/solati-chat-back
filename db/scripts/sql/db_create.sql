3738b48bb754e77af25b618a8c31a63bf1185c61
Joskar Hernandez <joskarandres97@gmail.com>
2020-12-17 12:33:10 -0500

    refactor: varias cosas

--- db/scripts/sql/db_create.sql
+++ db/scripts/sql/db_create.sql
@@ -2,6 +2,6 @@ create database chat_solati;
 
 CREATE USER solati_user WITH PASSWORD 'solati';
 
-GRANT ALL PRIVILEGES ON DATABASE "tuten_tests" to tuten_user;
+GRANT ALL PRIVILEGES ON DATABASE "chat_solati" to solati_user;
 
 \ c chat_solati;
